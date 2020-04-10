318654d15b22ee9d25c369ecc63e39f134e9c728
Shawn <shawn.gyina@student.kdg.be>
2020-04-10 16:57:27 +0200

    Add check if indexnames are present

--- test/ConfiguratorrTests/IndexerTests.cs
+++ test/ConfiguratorrTests/IndexerTests.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Configuratorr;
 using Configuratorr.IO;
@@ -10,9 +12,12 @@ namespace ConfiguratorrTests
     public class IndexerTests
     {
         private static readonly string jackettPath = AppDomain.CurrentDomain.BaseDirectory + "/Resources/Jackett/Indexers";
+        private static readonly List<string> indexerNames = new List<string>() {"1337x", "badasstorrents", "thepiratebay", "rarbg", "ettv"};
         [Fact]
         public void GetIndexerNamesFromFilePath() {
-            Assert.NotEmpty(IndexerPlugin.GetIndexerNamesFromPath(jackettPath));
+            var indexers = IndexerPlugin.GetIndexerNamesFromPath(jackettPath);
+            Assert.NotEmpty(indexers);
+            Assert.False(indexerNames.Except(indexers).Any());
         }
     }
 }
